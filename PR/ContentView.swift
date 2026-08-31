c606ba9ad0185fcb1022fc8c099809428ed49dc0
Jesus Almanza <c.jesusalmanza@gmail.com>
2026-08-31 17:58:05 -0600

    Initial Commit

---
+++ PR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PR
+//
+//  Created by Jesús Almanza on 31/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PR/PRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRApp.swift
+//  PR
+//
+//  Created by Jesús Almanza on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
